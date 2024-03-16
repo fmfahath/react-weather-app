a74841aae0688900b3e05f28418ef2ac77232f57
fmfahath <fmfahath.dev@gmail.com>
2024-03-16 12:19:49 +0530

    weather image, ions, text layout created

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -9,18 +9,51 @@ import rain_img from '../assets/rain.png'
 import search_img from '../assets/search.png'
 import snow_img from '../assets/snow.png'
 import wind_img from '../assets/wind.png'
+import { clear } from '@testing-library/user-event/dist/clear'
 
 const WeatherApp = () => {
     return (
-        //search bar
         <div className='container'>
+            {/* searchbar */}
             <div className="top-bar">
                 <input type="text" className="cityInput" placeholder='Search' />
                 <div className="search-icon">
                     <img src={search_icon} alt="" />
                 </div>
             </div>
+
+            {/* weather image */}
+            <div className="weather-image">
+                <img src={clear_img} alt="" />
+            </div>
+
+            {/* weather condition */}
+            <div className="weather-temp">25 °C</div>
+
+            {/* weather location */}
+            <div className="weather-location">London</div>
+
+            {/* humidity / wind  */}
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_img} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-per">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_img} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-per">85 Kmph</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+
     )
 }
 
