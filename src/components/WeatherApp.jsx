70e00ef8eb1ae4138a8f15ea1146d7566bc75bfb
fmfahath <fmfahath.dev@gmail.com>
2024-03-16 11:29:55 +0530

    boilerCode template created

---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './weatherApp.css'
+import search_icon from '../assets/search.png'
+import clear_img from '../assets/clear.png'
+import cloud_img from '../assets/cloud.png'
+import drizzle_img from '../assets/drizzle.png'
+import humidity_img from '../assets/humidity.png'
+import rain_img from '../assets/rain.png'
+import search_img from '../assets/search.png'
+import snow_img from '../assets/snow.png'
+import wind_img from '../assets/wind.png'
+
+const WeatherApp = () => {
+    return (
+        <div className='container'>WeatherApp</div>
+    )
+}
+
+export default WeatherApp
